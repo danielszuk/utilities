4fe2cd76a2acc93146a5a79692c3f2a82e07c688
Szük László Dániel <daniel.szuk@codelise.io>
2020-05-03 19:20:00 +0200

    FEATURE: add clone

---
+++ src/clone.spec.ts
@@ -0,0 +1,36 @@
+import { clone } from './clone';
+
+const simpleOriginal = { a: 1, b: 2 };
+const simpleReference = simpleOriginal;
+const simpleCloned = clone(simpleOriginal);
+
+test('Simple: Cloned object is not the same as original object', () => {
+  expect(simpleOriginal === simpleReference).toBe(true);
+  expect(simpleOriginal === simpleCloned).toBe(false);
+});
+
+test('Simple: Modifying the original object does not affect the cloned object', () => {
+  simpleOriginal.b = 27;
+  expect(simpleReference.b).toBe(27);
+  expect(simpleCloned.b).toBe(2);
+});
+
+const complexOriginal = { a: 1, b: { ba: 21, bb: 22 } };
+const complexReference = complexOriginal;
+const complexCloned = clone(complexOriginal);
+
+test('Complex: Cloned object is not the same as original object', () => {
+  expect(complexOriginal === complexReference).toBe(true);
+  expect(complexOriginal === complexCloned).toBe(false);
+  expect(complexOriginal.b === complexReference.b).toBe(true);
+  expect(complexOriginal.b === complexCloned.b).toBe(false);
+});
+
+test('Complex: Modifying the original object does not affect the cloned object', () => {
+  complexOriginal.a = 27;
+  expect(complexReference.a).toBe(27);
+  expect(complexCloned.a).toBe(1);
+  complexOriginal.b.ba = 42;
+  expect(complexReference.b.ba).toBe(42);
+  expect(complexCloned.b.ba).toBe(21);
+});
---
+++ src/clone.ts
@@ -0,0 +1,24 @@
+/**
+ * Creates a new object with the exact same values, but without the reference.
+ *
+ * @param object The original object to be cloned
+ * @returns a new object with the same attributes and corresponding values, but without the reference to the original object
+ */
+export const clone = <T>(object: T): T => {
+  if (!object || typeof object !== 'object') {
+    return object;
+  }
+
+  // @ts-ignore
+  const newObject = new object.constructor();
+
+  for (const attribute in object) {
+    if (typeof object[attribute] === 'object') {
+      newObject[attribute] = clone(object[attribute]);
+    } else {
+      newObject[attribute] = object[attribute];
+    }
+  }
+
+  return newObject;
+};
