f1d9c39a0c861ad8702de5b6ec000539e6bd1a66
Szük László Dániel <daniel.szuk@codelise.io>
2020-05-10 13:03:37 +0200

    FIX: test isEquivalent for non-equivalent objects as well

--- src/objects/isEquivalent.spec.ts
+++ src/objects/isEquivalent.spec.ts
@@ -1,5 +1,9 @@
 import { isEquivalent } from './isEquivalent';
 
+test('non-objects equality', () => {
+  expect(() => isEquivalent({ a: 1 }, 'a')).toThrow();
+});
+
 test('Two simple objects equality', () => {
   const obj1 = { a: 1 };
   const obj2 = { a: 1 };
@@ -18,4 +22,14 @@ test('Two complex objects equality', () => {
   expect(obj1 === obj2).toBe(false);
 
   expect(isEquivalent(obj1, obj2)).toBe(true);
+});
+
+test('Two different objects equality', () => {
+  // reference
+  expect(isEquivalent({ a: 1 }, { a: 1 })).toBe(true);
+
+  expect(isEquivalent({ a: 1 }, { a: 2 })).toBe(false);
+  expect(isEquivalent({ a: 1 }, { a: '1' })).toBe(false);
+  expect(isEquivalent({ a: 1, b: 2 }, { a: 1 })).toBe(false);
+  expect(isEquivalent({ a: 1, b: { ba: 21, bb: 22 } }, { a: 1, b: { ba: 21, bb: 41 } })).toBe(false);
 });
