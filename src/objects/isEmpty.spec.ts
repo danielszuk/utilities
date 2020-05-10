c22eb0120246d77ec01d82f7570fba8760dd53b8
Szük László Dániel <daniel.szuk@codelise.io>
2020-05-10 12:56:51 +0200

    FIX: test isEmpty for non-object and inherited properties

--- src/objects/isEmpty.spec.ts
+++ src/objects/isEmpty.spec.ts
@@ -1,7 +1,15 @@
 import { isEmpty } from './isEmpty';
 
+test('test a non-object', () => {
+  expect(() => isEmpty(27)).toThrow();
+});
+
 test('test an empty object', () => {
   expect(isEmpty({})).toBe(true);
+});
+
+test('test an empty object, which has only inherited attributes', () => {
+  expect(isEmpty(Object.create({ a: 1 }))).toBe(true);
 });
 
 test('test a non empty object', () => {
