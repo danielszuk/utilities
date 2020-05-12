6fc095d9868d3442f8671e341eb73fb2d26eb162
Szük László Dániel <daniel.szuk@codelise.io>
2020-05-12 21:06:30 +0200

    FIX(forEach): remove unnecessary async

--- src/forEaches/forEach.ts
+++ src/forEaches/forEach.ts
@@ -6,7 +6,7 @@
  * @param fn - runs the function once for all the array elements from the beginning.
  * You can break out of the iteration by returns false in this function.
  */
-export const forEach = async <T>(array: T[], fn: (element: T) => false | undefined | void): Promise<void> => {
+export const forEach = <T>(array: T[], fn: (element: T) => false | undefined | void): void => {
   const length = array.length;
   for (let i = 0; i < length; i++) {
     if (fn(array[i]) === false) {
@@ -23,7 +23,7 @@ export const forEach = async <T>(array: T[], fn: (element: T) => false | undefin
  * @param fn - runs the function once for all the array elements from the end.
  * You can break out of the iteration by returns false in this function
  */
-export const forEachBackward = async <T>(array: T[], fn: (element: T) => false | undefined | void): Promise<void> => {
+export const forEachBackward = <T>(array: T[], fn: (element: T) => false | undefined | void): void => {
   for (let i = array.length - 1; 0 <= i; i--) {
     if (fn(array[i]) === false) {
       break;
